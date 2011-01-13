93a3edd9d3fafa0b9603b67891b440f9b82fef9a
Simple Geebus <nerds+geebus@simplegeo.com>
2011-01-13 19:29:34 +0000

    Prepared changelog for build 13

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="dream",
-      version="1.0.12",
+      version="1.0.13",
       packages=find_packages(),
       install_requires=['webob',
                         'decoroute'],
