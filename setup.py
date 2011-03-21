251f653e42df5a654e87d8de9db82649a2579dfd
Simple Geebus <nerds+geebus@simplegeo.com>
2011-03-21 20:34:01 +0000

    Prepared changelog for build 28

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="dream",
-      version="1.0.27",
+      version="1.0.28",
       packages=find_packages(),
       install_requires=['webob',
                         'decoroute'],
