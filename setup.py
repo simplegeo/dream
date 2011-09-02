654469484382c0fdbc58ff5be712868aa32ba579
Simple Geebus <nerds+geebus@simplegeo.com>
2011-09-02 21:23:00 +0000

    Prepared changelog for build 79

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="dream",
-      version="1.0.78",
+      version="1.0.79",
       packages=find_packages(),
       install_requires=['webob',
                         'decoroute'],
